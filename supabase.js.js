ce5ab976d2b771586dff1888eaa0695e3207685a
nuha9-a11y <nuhanaif9@gmail.com>
2026-04-15 17:01:01 +0300

    Add files via upload

---
+++ supabase.js.js
@@ -0,0 +1,4 @@
+const supabaseUrl = "https://mcxdjisoyqdnyieboqpy.supabase.co";
+const supabaseKey = "sb_publishable_CoPFxO5-flUjMYi-jOdikw_kwAaUoZr";
+
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
